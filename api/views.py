ebc249a9159ee70c6df6ba6db7c9e9dee7d55962
S. M. JAHANGIR <smjxpro@gmail.com>
2020-07-11 10:54:06 +0600

    added serializers and viewsets

--- api/views.py
+++ api/views.py
@@ -1,2 +1,14 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from rest_framework import viewsets
+
+from api.models import Movie, Rating
+from api.serializers import MovieSerializer, RatingSerializer
+
+
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+
+class RatingViewSet(viewsets.ModelViewSet):
+    queryset = Rating.objects.all()
+    serializer_class = RatingSerializer

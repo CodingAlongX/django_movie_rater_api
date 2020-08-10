3d3f4dba53864241a6ac47a8ce7b194f0dceee8e
S. M. JAHANGIR <smjxpro@gmail.com>
2020-08-10 20:18:17 +0600

    fixed user registration permission

--- movie_rater_api/settings.py
+++ movie_rater_api/settings.py
@@ -120,9 +120,12 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': {
-        'rest_framework.permissions.IsAuthenticated',
-    }
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ),
 }
 
 CORS_ORIGIN_WHITELIST = [
